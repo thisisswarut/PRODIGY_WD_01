7ecd918b227b6924b22db6fb19e4cec3b14da402
V M Rutvik <157960688+thisisswarut@users.noreply.github.com>
2024-10-15 10:00:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Get the navbar
+const navbar = document.getElementById('navbar');
+
+// Add an event listener for scroll
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
